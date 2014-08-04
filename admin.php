415fcfa61202f90e654dc94620af5947b3a424d3
yospace <yospace@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-04 11:07:15 +0000

    Fix version number lossage
    git-svn-id: https://plugins.svn.wordpress.org/yoplayer/tags/2.1.30.3@959894 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin.php
@@ -0,0 +1,344 @@
+<?php
+/**
+ * @package yoplayer
+ * @version 2.1
+ *
+ * Copyright (C) 2013 - 2014 Yospace Technologies Ltd. All rights reserved
+ */
+
+/*===========================================================================*
+ * Add editor button(s)
+ *===========================================================================*/
+add_action('admin_print_footer_scripts', 'yoplayerAddQuicktags');
+function yoplayerAddQuicktags() {
+    if (wp_script_is('quicktags')) {
+?>
+<script type="text/javascript">
+QTags.addButton( 'yoplayer_add_player', 'yoplayer', '[yoplayer miid="', '"]', '', 'Yoplayer (VOD)', 250 );
+</script>
+<?php
+    }
+}
+add_filter('mce_buttons', 'yoplayerTinyMceButtons');
+function yoplayerTinyMceButtons($buttons) {
+    $buttons[] = 'yoplayer';
+    $buttons[] = 'cdslookup';
+    return $buttons;
+}
+
+/*===========================================================================*
+ * Process every page save and cache video metadata
+ *===========================================================================*/
+add_action('save_post', 'yoplayerCacheMetadata');
+function yoplayerCacheMetadata($post_id) {
+    global $wpdb, $yoplayer_table_name;
+    $content = $_POST['content'];
+    preg_match_all("/\[yoplayer[^]]*miid=[^[:digit:]\]]*(\d*)[^[:digit:]\]]*[^]]*\]/smx", $content, $matches);
+
+//    header("Content-Type: text/plain");
+//    print_r($matches);
+    $miids = array();
+    foreach($matches[1] as $miid) {
+        $miids[$miid] = 1;
+    }
+    foreach($miids as $miid => $value) {
+//        echo "$miid\n";
+        $metadata = yoplayerGetMetadata($miid);
+        if (count($metadata) == 0) {
+            continue;
+        }
+//        echo "Got metadata for $miid\n";
+        $wpdb->delete($yoplayer_table_name, array('miid' => $miid));
+        foreach($metadata as $field => $value) {
+            $wpdb->insert(
+                $yoplayer_table_name,
+                array(
+                    'miid' => $miid,
+                    'field' => $field,
+                    'value' => $value
+                )
+            );
+//            echo "$miid : $field => $value\n";
+        }
+    }
+//    die();
+}
+
+function yoplayerDoPostRequest($url, $data, $optional_headers = null) {
+    $params = array('http' => array(
+        'method' => 'POST',
+        'content' => $data
+    ));
+    if ($optional_headers !== null) {
+        $params['http']['header'] = $optional_headers;
+    }
+    $ctx = stream_context_create($params);
+    $fp = @fopen($url, 'rb', false, $ctx);
+    if (!$fp) {
+        throw new Exception("Problem with $url, $php_errormsg");
+    }
+    $response = @stream_get_contents($fp);
+    if ($response === false) {
+        throw new Exception("Problem reading data from $url, $php_errormsg");
+    }
+    return $response;
+}
+
+function yoplayerGetMetadata($miid) {
+    $CLIENT_UN = get_option("yoplayer-username", '');
+    $CLIENT_PW = get_option("yoplayer-password", '');
+    $SOAPOUT = '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+  <ns2:getMetaDataFields xmlns:ns2="http://www.yospace.com/tundra/MediaItemManagement/">
+    <username>' . $CLIENT_UN . '</username>
+    <password>' . $CLIENT_PW . '</password>
+    <mediaItemId>' . $miid . '</mediaItemId>
+  </ns2:getMetaDataFields>
+  </soap:Body>
+</soap:Envelope>';
+    $xmlresult = simplexml_load_string(str_replace('ns2:', '', str_replace('soap:', '', yoplayerDoPostRequest("http://cds1.yospace.com/mediaitemmanager/manage", $SOAPOUT, array(
+        "SOAPAction" => "http://www.yospace.com/tundra/MediaItemManagement",
+        "Content-Type" => "text/xml;charset=UTF-8"
+    )))));
+    $retval = array();
+    $result = $xmlresult->Body[0]->getMetaDataFieldsResponse[0];
+    if ($result->statusCode[0] == "200") {
+        foreach($result->mediaItemFields as $mediaItemField) {
+            $retval[(string)$mediaItemField->fieldName] = (string)$mediaItemField->value;
+        }
+    }
+    return $retval;
+}
+
+/*===========================================================================*
+ * Construct options page and link in menu
+ *===========================================================================*/
+add_action('admin_menu', 'yoplayer_admin_menu');
+function yoplayer_admin_menu() {
+    add_action( 'admin_init', 'yoplayer_init' );
+    add_options_page('Yoplayer Defaults', 'Yoplayer Defaults', 'manage_options', 'yoplayer-defaults', 'yoplayer_defaults');
+}
+
+function yoplayer_init() {
+    register_setting("yoplayer-defaults", "yoplayer-fid");
+    register_setting("yoplayer-defaults", "yoplayer-width");
+    register_setting("yoplayer-defaults", "yoplayer-height");
+    register_setting("yoplayer-defaults", "yoplayer-skin");
+    register_setting("yoplayer-defaults", "yoplayer-custom-skin");
+    register_setting("yoplayer-defaults", "yoplayer-panning");
+    register_setting("yoplayer-defaults", "yoplayer-enablecc");
+    register_setting("yoplayer-defaults", "yoplayer-debug");
+    register_setting("yoplayer-defaults", "yoplayer-autoplay");
+    register_setting("yoplayer-defaults", "yoplayer-buffer");
+    register_setting("yoplayer-defaults", "yoplayer-lwm");
+    register_setting("yoplayer-defaults", "yoplayer-lss");
+    register_setting("yoplayer-defaults", "yoplayer-username");
+    register_setting("yoplayer-defaults", "yoplayer-password");
+    register_setting("yoplayer-defaults", "yoplayer-metadata");
+    add_settings_section(
+        "yoplayer-defaults-cds",
+        "Yospace CDS Settings",
+        "yoplayer_presentation_header",
+        "yoplayer-defaults"
+    );
+    add_settings_field("yoplayer-fid", "Feed ID (fid)", "yoplayer_field_fid", "yoplayer-defaults", "yoplayer-defaults-cds");
+    add_settings_section(
+        "yoplayer-defaults-presentation",
+        "Presentation",
+        "yoplayer_presentation_header",
+        "yoplayer-defaults"
+    );
+    add_settings_field("yoplayer-width", "Width (width)", "yoplayer_field_width", "yoplayer-defaults", "yoplayer-defaults-presentation");
+    add_settings_field("yoplayer-height", "Height (height)", "yoplayer_field_height", "yoplayer-defaults", "yoplayer-defaults-presentation");
+    add_settings_field("yoplayer-skin", "Skin URL (skin)", "yoplayer_field_skin", "yoplayer-defaults", "yoplayer-defaults-presentation");
+    add_settings_field("yoplayer-panning", "Panning (panning)", "yoplayer_field_panning", "yoplayer-defaults", "yoplayer-defaults-presentation");
+    add_settings_field("yoplayer-enablecc", "Closed Captions (enablecc)", "yoplayer_field_enablecc", "yoplayer-defaults", "yoplayer-defaults-presentation");
+    add_settings_field("yoplayer-debug", "Debugging (debug)", "yoplayer_field_debug", "yoplayer-defaults", "yoplayer-defaults-presentation");
+    add_settings_field("yoplayer-autoplay", "Auto Play (autoplay)", "yoplayer_field_autoplay", "yoplayer-defaults", "yoplayer-defaults-presentation");
+    add_settings_section(
+        "yoplayer-defaults-playback",
+        "Plaback Control",
+        "yoplayer_playback_header",
+        "yoplayer-defaults"
+    );
+    add_settings_field("yoplayer-buffer", "Buffer (buffer)", "yoplayer_field_buffer", "yoplayer-defaults", "yoplayer-defaults-playback");
+    add_settings_field("yoplayer-lwm", "Low Water Mark (lwm)", "yoplayer_field_lwm", "yoplayer-defaults", "yoplayer-defaults-playback");
+    add_settings_field("yoplayer-lss", "Live Stream Start (lss)", "yoplayer_field_lss", "yoplayer-defaults", "yoplayer-defaults-playback");
+    add_settings_section(
+        "yoplayer-defaults-metadata",
+        "Download Metadata yospaceCDS",
+        "yoplayer_metadata_header",
+        "yoplayer-defaults"
+    );
+    add_settings_field("yoplayer-username", "CDS Login", "yoplayer_field_username", "yoplayer-defaults", "yoplayer-defaults-metadata");
+    add_settings_field("yoplayer-password", "CDS Password", "yoplayer_field_password", "yoplayer-defaults", "yoplayer-defaults-metadata");
+    add_settings_field("yoplayer-metadata", "Default Metadata Keys", "yoplayer_field_metadata", "yoplayer-defaults", "yoplayer-defaults-metadata");
+}
+
+function yoplayer_cds_header() {
+    echo "<div>Default values from your Yospace CDS account.</div>";
+}
+
+function yoplayer_field_fid() {
+    echo '<input type="text" name="yoplayer-fid" id="yoplayer-fid" size="50" class="regular-text" value="' . get_option("yoplayer-fid", '') . '" />';
+}
+
+function yoplayer_presentation_header() {
+    echo "<div>Options to alter how Yoplayer appears in your pages.</div>";
+}
+
+function yoplayer_field_width() {
+    echo '<input type="text" name="yoplayer-width" id="yoplayer-width" size="50" class="regular-text" value="' . get_option("yoplayer-width", 640) . '" />';
+}
+
+function yoplayer_field_height() {
+    echo '<input type="text" name="yoplayer-height" id="yoplayer-height" class="regular-text" value="' . get_option("yoplayer-height", 352) . '" />';
+}
+
+function yoplayer_field_skin() {
+    echo '<input type="radio" name="yoplayer-skin" id="yoplayer-skin-default" value=""';
+    if (get_option("yoplayer-skin", "") == "") {
+        echo " checked";
+    }
+    echo ' /> <label for="yoplayer-skin-default">Default</label> <i style="font-size: smaller">(HLS-SDK Default Skin)</i><br/>';
+    echo '<input type="radio" name="yoplayer-skin" id="yoplayer-skin-blacklight" value="Black-Light.skin"';
+    if (get_option("yoplayer-skin", "") == "Black-Light.skin") {
+        echo " checked";
+    }
+    echo ' /> <label for="yoplayer-skin-blacklight">Black-Light</label> <i style="font-size: smaller">(Floating, auto-hide control bar with full controls)</i><br/>';
+    echo '<input type="radio" name="yoplayer-skin" id="yoplayer-skin-minimal" value="Minimal.skin"';
+    if (get_option("yoplayer-skin", "") == "Minimal.skin") {
+        echo " checked";
+    }
+    echo ' /> <label for="yoplayer-skin-minimal">Minimal</label> <i style="font-size: smaller">(Floating, auto-dim control bar with minimal controls)</i><br/>';
+    echo '<input type="radio" name="yoplayer-skin" id="yoplayer-skin-fixed" value="Fixed.skin"';
+    if (get_option("yoplayer-skin", "") == "Fixed.skin") {
+        echo " checked";
+    }
+    echo ' /> <label for="yoplayer-skin-fixed">Fixed</label> <i style="font-size: smaller">(Fixed control bar below video - Allow an additional 32px height)</i><br/>';
+    echo '<input type="radio" name="yoplayer-skin" id="yoplayer-skin-custom" value="custom"';
+    if (get_option("yoplayer-skin", "") == "custom") {
+        echo " checked";
+    }
+    echo ' /> <label for="yoplayer-skin-custom">Custom:</label> ';
+    echo '<input type="text" name="yoplayer-custom-skin" id="yoplayer-custom-skin" class="regular-text" value="' . get_option("yoplayer-custom-skin", "int://internal.skin") . '"';
+    if (get_option("yoplayer-skin", "") != "custom") {
+        echo " disabled";
+    }
+    echo ' />';
+}
+
+function yoplayer_field_panning() {
+    echo '<input type="checkbox" name="yoplayer-panning" id="yoplayer-panning" value="true"';
+    if (get_option("yoplayer-panning", "false") == "true") {
+        echo " checked";
+    }
+    echo ' />';
+}
+
+function yoplayer_field_enablecc() {
+    echo '<input type="checkbox" name="yoplayer-enablecc" id="yoplayer-enablecc" value="true"';
+    if (get_option("yoplayer-enablecc", "false") == "true") {
+        echo " checked";
+    }
+    echo ' />';
+}
+
+function yoplayer_field_debug() {
+    echo '<input type="checkbox" name="yoplayer-debug" id="yoplayer-debug" value="true"';
+    if (get_option("yoplayer-debug", "false") == "true") {
+        echo " checked";
+    }
+    echo ' />';
+}
+
+function yoplayer_field_autoplay() {
+    echo '<input type="checkbox" name="yoplayer-autoplay" id="yoplayer-autoplay" value="true"';
+    if (get_option("yoplayer-autoplay", "false") == "true") {
+        echo " checked";
+    }
+    echo ' />';
+}
+
+function yoplayer_playback_header() {
+    echo "<div>Options to alter how Yoplayer downloads and plays your videos.</div>";
+    echo "<div>These are advanced features and should only be changed after reading the HLS-SDK documentation.</div>";
+}
+
+function yoplayer_field_buffer() {
+    echo '<input type="text" name="yoplayer-buffer" id="yoplayer-buffer" size="50" class="regular-text" value="' . get_option("yoplayer-buffer", 30) . '" />';
+}
+
+function yoplayer_field_lwm() {
+    echo '<input type="text" name="yoplayer-lwm" id="yoplayer-lwm" class="regular-text" value="' . get_option("yoplayer-lwm", 5) . '" />';
+}
+
+function yoplayer_field_lss() {
+    echo '<input type="text" name="yoplayer-lss" id="yoplayer-lss" class="regular-text" value="' . get_option("yoplayer-lss", 3) . '" />';
+}
+
+function yoplayer_metadata_header() {
+    echo "<div>To download metadata for each video from yospaceCDS, enter your yospaceCDS username and password here. (Optional)</div>";
+}
+
+function yoplayer_field_username() {
+    echo '<input type="text" name="yoplayer-username" id="yoplayer-username" size="50" class="regular-text" value="' . get_option("yoplayer-username", '') . '" />';
+}
+
+function yoplayer_field_password() {
+    echo '<input type="password" name="yoplayer-password" id="yoplayer-password" class="regular-text" value="' . get_option("yoplayer-password", '') . '" />';
+}
+
+function yoplayer_field_metadata() {
+    echo '<input type="text" name="yoplayer-metadata" id="yoplayer-metadata" class="regular-text" value="' . get_option("yoplayer-metadata", '') . '" />';
+}
+
+function yoplayer_defaults() {
+?>
+<div class="wrap">
+<style type="text/css">
+input[type="text"][disabled] {
+    background-color: #e0e0e0;
+}
+</style>
+<?php screen_icon(); ?>
+<h2>Yoplayer Defaults</h2>
+<div>Each option shown has the name of the shortcode parameter in brackets
+    after. The value shown here will be used unless you override it in the
+    shortcode.</div>
+<div>For example <code>[yoplayer miid="123456"]</code> will take all the values
+    shown below, while <code>[yoplayer miid="123456" fid="987654" width="320" height="176"]</code>
+    will override just the on-screen dimensions of the player.</div>
+<form method="post" action="options.php">
+    <?php settings_fields('yoplayer-defaults'); ?>
+    <?php do_settings_sections('yoplayer-defaults'); ?>
+    <?php submit_button(); ?>
+</form>
+<p>Don&apos;t forget to upload your <code>yospace.lic</code> file to the root of
+    your webserver, e.g. it must be available as <code>/yospace.lic</code>.
+</div>
+<script type="text/javascript">
+var rbDefault = document.getElementById("yoplayer-skin-default");
+var rbBlackLight = document.getElementById("yoplayer-skin-blacklight");
+var rbMinimal = document.getElementById("yoplayer-skin-minimal");
+var rbFixed = document.getElementById("yoplayer-skin-fixed");
+var rbCustom = document.getElementById("yoplayer-skin-custom");
+var txtCustom = document.getElementById("yoplayer-custom-skin");
+rbDefault.addEventListener("click", function() {
+    txtCustom.setAttribute("disabled", true); }
+);
+rbBlackLight.addEventListener("click", function() {
+    txtCustom.setAttribute("disabled", true); }
+);
+rbMinimal.addEventListener("click", function() {
+    txtCustom.setAttribute("disabled", true); }
+);
+rbFixed.addEventListener("click", function() {
+    txtCustom.setAttribute("disabled", true); }
+);
+rbCustom.addEventListener("click", function() {
+    txtCustom.removeAttribute("disabled"); }
+);
+</script>
+<?php
+}
---
+++ yoplayer.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * @package yoplayer
+ * @version 2.1.30.3
+ *
+ * Copyright (C) 2013 - 2014 Yospace Technologies Ltd. All rights reserved
+ */
+/*
+Plugin Name: Yoplayer
+Plugin URI: http://www.yospace.com/index.php/hls-sdk-for-flash-overview.html
+Description: Yospace, the leader in n-screen delivery, present Yoplayer based on their world beating Flash HLS-SDK technology allowing playback of the widest range of video formats in a single player. Coupled with this is out of the box support for Google IMA for content monetization. Yoplayer for Wordpress allows you to provide a Yospace Media Item ID and Feed ID from your Yospace account and it takes care of selecting the appropriate video format, metadata (title, description etc) with any further user configuration.
+Author: Yospace Technologies Ltd
+Author URI: http://www.yospace.com
+Version: 2.1.30.3
+Licence: GPLv2 or later
+*/
+
+global $wpdb, $yoplayer_table_name;
+$yoplayer_table_name = $wpdb->prefix . "yoplayer_metadata";
+
+// Make sure we don't expose any info if called directly
+if ( !function_exists( 'add_action' ) ) {
+    echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+    exit;
+}
+
+define('YOPLAYER_VERSION', '2.1');
+define('YOPLAYER_PLUGIN_URL', plugin_dir_url( __FILE__ ));
+
+/*===========================================================================*
+ * Include admin.php only if we are in the admin interface
+ *===========================================================================*/
+if ( is_admin() )
+    require_once dirname( __FILE__ ) . '/admin.php';
+
+/*===========================================================================*
+ * Render the tag
+ *===========================================================================*/
+add_action('wp_enqueue_scripts','yoplayerScripts',0);
+function yoplayerScripts() {
+    wp_enqueue_script("yoplayer", plugins_url('js/yoplayer-' . YOPLAYER_VERSION . '.js', __FILE__));
+}
+
+$players = array();
+add_shortcode('yoplayer', 'yoplayerEmbed');
+function yoplayerEmbed($atts) {
+    global $players, $wpdb, $yoplayer_table_name;
+    $id = 1;
+    $values = shortcode_atts(array(
+        'miid' => '',
+        'live' => '',
+        'pp' => 'm3u8',
+        'bsid' => '',
+        'url' => '',
+        'fid' => get_option('yoplayer-fid', ''),
+        'skin' => '',
+        'width' => get_option('yoplayer-width', '640'),
+        'height' => get_option('yoplayer-height', '352'),
+        'buffer' => get_option('yoplayer-buffer', '30'),
+        'lwm' => get_option('yoplayer-lwm', '5'),
+        'lss' => get_option('yoplayer-lss', '3'),
+        'panning' => get_option('yoplayer-panning', false),
+        'enablecc' => get_option('yoplayer-enablecc', false),
+        'debug' => get_option('yoplayer-debug', false),
+        'autoplay' => get_option('yoplayer-autoplay', false),
+        'startlevel' => null,
+        'metadata' => get_option('yoplayer-metadata', '')
+    ), $atts);
+    foreach (array('panning', 'enablecc', 'debug', 'autoplay') as $key) {
+        if (($values[$key] == 1) ||
+            ($values[$key] == '1') ||
+            ($values[$key] == true) ||
+            ($values[$key] == 'true') ||
+            ($values[$key] == 'yes')) {
+            $values[$key] = 'true';
+        } else {
+            $values[$key] = 'false';
+        }
+    }
+    if ($values['skin'] == "") {
+        $skin_setting = get_option('yoplayer-skin', '');
+        if ($skin_setting == 'custom') {
+            $values['skin'] = get_option('yoplayer-custom-skin', '');
+        } else if ($skin_setting == "") {
+        } else {
+            $values['skin'] = plugins_url('skins/' . $skin_setting, __FILE__);
+        }
+    }
+    if ($values['miid'] == '') {
+        $values['miid'] = $values['bsid'];
+    }
+    while (array_key_exists($values['miid'] . '_' . $id, $players)) {
+        $id++;
+    }
+    $thisID = $values['miid'] . '_' . $id;
+    $retval = '<script type="text/javascript">
+$YOPLAYERHERE(
+    "yoplayer_' . $thisID . '",
+    {
+        "player": "' . plugins_url('player/yoplayer-' . YOPLAYER_VERSION . '.swf', __FILE__) . '",
+        "width": ' . $values['width'] .',
+        "height": ' . $values['height'] .',
+        "buffer": ' . $values['buffer'] . ',
+        "lwm": ' . $values['lwm'] . ',
+        "lss": ' . $values['lss'] . ',
+        "panning": ' . $values['panning'] . ',
+        "enablecc": ' . $values['enablecc'] . ',
+        "debug": ' . $values['debug'] . ',
+        "autoplay": ' . $values['autoplay'] . ',
+        "startlevel": ' . $values['startlevel'];
+    if ($values['bsid']) {
+        $retval .= ',
+        "live": true,
+        "miid": "' . $values['bsid'] . '",
+        "pp": "m3u8"';
+    } else {
+        $retval .= ',
+        "miid": "' . $values['miid'] . '"';
+        if ($values['live']) {
+            $retval .= ',
+        "live": true,
+        "pp": "' . $values['pp'] . '"';
+        } else {
+            $retval .= ',
+        "fid": "' . $values['fid'] . '"';
+        }
+    }
+    if ($values['skin']) {
+        $retval .= ',
+        "skin": "' . $values['skin'] . '"';
+    }
+    $retval .= '
+    }
+);
+</script>
+';
+    $players[$thisID] = 1;
+    if (!$values['miid'] && !$values['url']) {
+        return "<div>No video supplied to <code>yoplayer</code> tag</div>";
+    }
+    $metakeys = explode(",", $values['metadata']);
+    if ((count($metakeys) > 1) || (strlen($metakeys[0]) > 0)) {
+        foreach($metakeys as $key) {
+            $value = $wpdb->get_var("SELECT value FROM $yoplayer_table_name WHERE miid='" . $values['miid'] . "' AND field='" . trim($key) . "'");
+            $retval .= '<div class="yoplayer_' . str_replace(' ', '_', trim($key)) . '" id="yoplayer' . $thisID .'_' . str_replace(' ', '_', trim($key)) . '">' . $value . "</div>\n";
+        }
+    }
+    return $retval;
+}
+
+/*===========================================================================*
+ * Plugin Activation
+ *===========================================================================*/
+register_activation_hook(__FILE__, 'yoplayerActivate');
+function yoplayerActivate() {
+    global $yoplayer_table_name;
+    $sql = "CREATE TABLE $yoplayer_table_name (
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  miid VARCHAR(255) NOT NULL,
+  field VARCHAR(255) NOT NULL,
+  value VARCHAR(4096) DEFAULT '',
+  PRIMARY KEY  (id)
+);";
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+}
